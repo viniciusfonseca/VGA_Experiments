5d95ce0fdac5d13c72b4edec38b82ca24c46ffde
Vinicius Fonseca <vfonseca1618@gmail.com>
2018-05-31 22:48:34 -0300

    fix indent

--- VGA_TEST.C
+++ VGA_TEST.C
@@ -12,17 +12,17 @@ void main()
 
     for (i = 0; i < 4; i++)
     {
-	if (c >= 256) break;
-	for (j = 0; j < 80; j++)
-	{
-	    c++;
-	    if (c >= 256) break;
+		if (c >= 256) break;
+		for (j = 0; j < 80; j++)
+		{
+			c++;
+			if (c >= 256) break;
 
-	    plot_pixel(j*2  , i*2  , c);
-	    plot_pixel(j*2+1, i*2  , c);
-	    plot_pixel(j*2  , i*2+1, c);
-	    plot_pixel(j*2+1, i*2+1, c);
-	}
+			plot_pixel(j*2  , i*2  , c);
+			plot_pixel(j*2+1, i*2  , c);
+			plot_pixel(j*2  , i*2+1, c);
+			plot_pixel(j*2+1, i*2+1, c);
+		}
     }
 
     getchar();
